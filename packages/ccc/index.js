77718247d06dcc313ac807031cca599480107291
Olzhas Seitmagambet <sei7magambet@gmail.com>
2020-11-11 23:50:36 +0600

    add ccc

---
+++ packages/ccc/index.js
@@ -0,0 +1,8 @@
+import { aaa } from '@winexy/aaa';
+import { bbb } from '@winexy/bbb';
+
+function ccc() {
+  return aaa() + bbb();
+}
+
+export { ccc };
---
+++ packages/ccc/index.test.js
@@ -0,0 +1,7 @@
+import { ccc } from '.';
+
+describe('ccc', () => {
+  it('should', () => {
+    expect(ccc()).toBe('aaabbb');
+  });
+});
